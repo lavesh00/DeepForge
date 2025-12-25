3ab1c4fed1df0b8e7d9736be6a301a0a8236c1cf
Lavesh Patil <laveshpatil103@gmail.com>
2025-12-25 17:57:00 +0530

    Add core/bootstrap/__init__.py

---
+++ core/bootstrap/__init__.py
@@ -0,0 +1,8 @@
+"""Bootstrap module for system initialization."""
+
+from .env_detect import detect_environment
+from .first_run import FirstRunWizard
+
+__all__ = ["detect_environment", "FirstRunWizard"]
+
+
