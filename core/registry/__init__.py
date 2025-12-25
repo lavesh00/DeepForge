1a6b4e318787d1bb81edd2cc2a8a89ba83f6421a
Lavesh Patil <laveshpatil103@gmail.com>
2025-12-25 17:56:18 +0530

    Add core/registry/__init__.py

---
+++ core/registry/__init__.py
@@ -0,0 +1,7 @@
+"""Service registry module."""
+
+from .service_registry import ServiceRegistry, get_service, register_service
+
+__all__ = ["ServiceRegistry", "get_service", "register_service"]
+
+
